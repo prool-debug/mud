0202589067f8093a67bd1924db98455a78c2bb8a
Serhii <prool@users.noreply.github.com>
2026-04-19 11:14:03 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,4 +4,6 @@ int main(const int argc, char **argv) {
 	return main_function(argc, argv);
 }
 
+// 19.04.2026. prool experiment. change this source file. CI started?
+
 // vim: ts=4 sw=4 tw=0 noet syntax=cpp :
